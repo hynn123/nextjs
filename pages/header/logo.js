5faf4264af1e30bdb99b8aff7ce9c0e115bd0566
Hynn <hynn@live.jp>
2020-12-16 08:40:34 -0300

    Testando inserção de component

---
+++ pages/header/logo.js
@@ -0,0 +1,19 @@
+import react from 'react';
+import Head from 'next/head';
+
+class Logo extends Component 
+{
+	render()
+	{
+		<div>
+			<Head>
+			<title>Lobato Developer</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		
+		<div>Logo</div>
+		</div>
+	}
+}
+
+export default Logo;
