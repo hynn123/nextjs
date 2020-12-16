1f1c34191c570b337fe052392e88026f4f5a2e21
Hynn <hynn@live.jp>
2020-12-16 00:19:19 -0300

    Testando mudanças

--- pages/index.js
+++ pages/index.js
@@ -9,10 +9,23 @@ export default function Home()
         <title>Lobato Developer</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      
-      <div class="container">
-        <div class="mx-auto">Lobato</div>
-      </div>
+
+      <main className={styles.main}>
+        <div class="container">
+          <div class="mx-auto">Lobato</div>
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by{' '}
+          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        </a>
+      </footer>
     </div>
   );
 }
