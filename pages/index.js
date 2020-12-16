a070bb02c0619226169f410d6abd5634229ccfae
Hynn <hynn@live.jp>
2020-12-16 00:27:12 -0300

    Testando mudanças

--- pages/index.js
+++ pages/index.js
@@ -10,17 +10,14 @@ export default function Home()
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main class="w-screen h-screen flex justify-items-center align-center">
-        <div class="container">
+      <main>
+        <section class="container">
           <div class="mx-auto">Lobato</div>
-        </div>
+        </section>
       </main>
 
       <footer className={styles.footer}>
-        <a href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" target="_blank" rel="noopener noreferrer">
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+        
       </footer>
     </div>
   );
